7aa4d980d1c51a7fc534237cd1dddce319f1b1db
ayoubyt <ayoub.aguert@gmail.com>
2020-12-23 00:30:18 +0100

    working on disjoint pattern db

--- src/pattern_databases/Disjoint_database.cpp
+++ src/pattern_databases/Disjoint_database.cpp
@@ -1,10 +1,26 @@
 #include "Disjoint_database.hpp"
+#include "Board.class.hpp"
+
+const Board sb_663_p1({1, 2, 3 - 1,
+                       12, -1, -1, -1,
+                       11, 0, -1, -1,
+                       10, -1, -1, -1});
+
+const Board sb_663_p2({-1, -1, -1, 4,
+                       -1, -1, -1, 5,
+                       -1, 0, -1, 6,
+                       -1, 9, 8, 7});
+
+const Board sb_663_p3({-1, -1, -1, -1,
+                       -1, 13, 14, -1,
+                       -1, 0, 15, -1,
+                       -1, -1, -1, -1});
 
 std::vector<std::pair<int, int>> make_entries(){
+
 
 };
 
 void DB_663::create()
 {
-
 }
