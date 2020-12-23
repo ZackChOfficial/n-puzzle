b867455585aadaac7f6118041ecb31edd5bd9e72
ayoubyt <ayoub.aguert@gmail.com>
2020-12-23 14:04:16 +0100

    working on wdb

--- src/pattern_databases/Disjoint_database.cpp
+++ src/pattern_databases/Disjoint_database.cpp
@@ -1,5 +1,8 @@
+#include <queue>          // std::queue
+
 #include "Disjoint_database.hpp"
 #include "Board.class.hpp"
+
 
 const Board sb_663_p1({1, 2, 3 - 1,
                        12, -1, -1, -1,
@@ -17,8 +20,9 @@ const Board sb_663_p3({-1, -1, -1, -1,
                        -1, -1, -1, -1});
 
 std::vector<std::pair<int, int>> make_entries(){
+    // std<vector>
 
-
+    // std::queue <>
 };
 
 void DB_663::create()
